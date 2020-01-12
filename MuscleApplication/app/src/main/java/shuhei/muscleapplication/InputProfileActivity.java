cacb9d8be060c277b0cc1aa6d0db3d8e56f124e3
tshuhei <stsuriya@ucdavis.edu>
2020-01-12 13:57:24 -0800

    Ver3 implemented uploading to database

--- MuscleApplication/app/src/main/java/shuhei/muscleapplication/InputProfileActivity.java
+++ MuscleApplication/app/src/main/java/shuhei/muscleapplication/InputProfileActivity.java
@@ -3,13 +3,40 @@ package shuhei.muscleapplication;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class InputProfileActivity extends AppCompatActivity {
     Button next;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
+    private RadioGroup userTypeRadioGroup;
+    private RadioGroup genderRadioGroup;
+    private RadioGroup workoutExperienceRadioGroup;
+    private EditText inputHeight;
+    private EditText inputWeight;
+    private EditText inputNickname;
+    private EditText inputIntroduction;
+    private String strUserType;
+    private String strGender;
+    private String strWorkoutExperience;
+    private String strHeight;
+    private String strWeight;
+    private String strNickname;
+    private String strIntroduction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +48,63 @@ public class InputProfileActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                strHeight = inputHeight.getText().toString();
+                strWeight = inputWeight.getText().toString();
+                strNickname = inputNickname.getText().toString();
+                strIntroduction = inputIntroduction.getText().toString();
+
+                mFirebaseAuth = FirebaseAuth.getInstance();
+                mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                if(mFirebaseUser == null){
+                    Log.println(0,"login","user not login");
+                }else{
+                    mUserId = mFirebaseUser.getUid();
+                    mDatabase.child("users").child(mUserId).child("items").child("usertype").setValue(strUserType);
+                    mDatabase.child("users").child(mUserId).child("items").child("gender").setValue(strGender);
+                    mDatabase.child("users").child(mUserId).child("items").child("experience").setValue(strWorkoutExperience);
+                    mDatabase.child("users").child(mUserId).child("items").child("height").setValue(strHeight);
+                    mDatabase.child("users").child(mUserId).child("items").child("weight").setValue(strWeight);
+                    mDatabase.child("users").child(mUserId).child("items").child("nickname").setValue(strNickname);
+                    mDatabase.child("users").child(mUserId).child("items").child("introduction").setValue(strIntroduction);
+                }
+
                 loadImputPhotoView();
             }
         });
 
+        userTypeRadioGroup = (RadioGroup)findViewById(R.id.userTypeRadioGroup);
+        genderRadioGroup = (RadioGroup)findViewById(R.id.genderRadioGroup);
+        workoutExperienceRadioGroup = (RadioGroup)findViewById(R.id.workoutExperienceRadioGroup);
+        inputHeight = (EditText)findViewById(R.id.inputHeight);
+        inputWeight = (EditText)findViewById(R.id.inputWeight);
+        inputNickname = (EditText)findViewById(R.id.inputNickname);
+        inputIntroduction = (EditText)findViewById(R.id.inputIntroduction);
+
+        userTypeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton userType = (RadioButton)findViewById(i);
+                strUserType = userType.getText().toString();
+            }
+        });
+
+        genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton gender = (RadioButton)findViewById(i);
+                strGender = gender.getText().toString();
+            }
+        });
+
+        workoutExperienceRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton workoutExperience = (RadioButton)findViewById(i);
+                strWorkoutExperience = workoutExperience.getText().toString();
+            }
+        });
     }
 
     public void loadImputPhotoView(){
--- MuscleApplication/app/src/main/java/shuhei/muscleapplication/MainActivity.java
+++ MuscleApplication/app/src/main/java/shuhei/muscleapplication/MainActivity.java
@@ -2,35 +2,81 @@ package shuhei.muscleapplication;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener,
+MatchFragment.OnFragmentInteractionListener, ChatFragment.OnFragmentInteractionListener, RewardFragment.OnFragmentInteractionListener{
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(R.string.home);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
+        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch(menuItem.getItemId()){
+                    case R.id.navigation_home:
+                        toolbar.setTitle("Home");
+                        fragment = new HomeFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_profile:
+                        toolbar.setTitle("Profile");
+                        fragment = new ProfileFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_match:
+                        toolbar.setTitle("Match");
+                        fragment = new MatchFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_chat:
+                        toolbar.setTitle("Chat");
+                        fragment = new ChatFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.navigation_award:
+                        toolbar.setTitle("Award");
+                        fragment = new RewardFragment();
+                        loadFragment(fragment);
+                        return true;
+
+                }
+                return true;
+            }
+        };
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
         if(mFirebaseUser == null){
             loadLogInView();
         }
+        loadFragment(new HomeFragment());
     }
 
     @Override
@@ -61,5 +107,17 @@ public class MainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
